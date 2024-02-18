1e5d3e86aacbe4e3cc986be633ab3a9e7a500cac
Forzana0 <mrhungie414@gmail.com>
2024-02-18 17:35:07 +0200

    1

--- 1.bot/handlers/user_group.py
+++ 1.bot/handlers/user_group.py
@@ -1,10 +1,20 @@
 from filter.chat_types import ChatTypeFilter
-
+from string import punctuation
 from aiogram import Router, types
 user_group_router = Router()
 user_group_router.message.filter(ChatTypeFilter(['group', 'supergroup']))
 
+banned_words={'ку','прив', 'пон', 'зроз'}
 
+def clean_text(text:str):
+    return text.translate(str.maketrans('','', punctuation)).lower()
+
+@user_group_router.edited_message()
 @user_group_router.message()
 async def group_message(message: types.Message):
-    await message.reply(f"Hello user {message.from_user.full_name}, {message.text}")
+    if banned_words.intersection(message.text.lower().split()):
+        await message.reply(clean_text(message.text))
+        await message.answer(f"{message.from_user.full_name}, keep calm in the chat!")
+    else:
+        await message.answer(f"Hello to you{message.from_user.full_name}")
+    # await message.reply(f"Hello user {message.from_user.full_name}, {message.text}")
